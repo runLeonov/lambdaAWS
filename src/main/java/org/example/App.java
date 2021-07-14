239620c142eb2b7a2548131455815ee209019c8d
runLeonov <leonovcasha@gmail.com>
2021-07-14 17:32:33 +0300

    kaif

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,45 +9,17 @@ import java.util.Map;
 /**
  * Hello world!
  */
-public class App implements RequestHandler<Map<String, String>, String> {
+public class App implements RequestHandler<Map<String, String>,  Map<String, String>> {
     @Override
-    public String handleRequest(Map<String, String> data, Context context) {
-        JSONObject jsonObject = new JSONObject();
+    public Map<String, String> handleRequest(Map<String, String> data, Context context) {
         if (!data.containsKey("message")) {
-            jsonObject.append("error", "Field message can not be null or empty");
-            return jsonObject.toString();
+            return Map.of("error", "Field message can not be null or empty");
         }
         StringBuilder sb = new StringBuilder(data.get("message"));
         for (int i = 1; i < sb.toString().toCharArray().length; i++) {
             sb.deleteCharAt(i);
         }
-        jsonObject.put("message", sb.toString());
-        System.out.println(jsonObject);
         System.out.println("Hello from my lambda function!");
-        return jsonObject.toString();
+        return Map.of("message", sb.toString());
     }
-
-//    public static void main(String[] args) {
-//        Map<String, String> data = new HashMap<>();
-//        data.put("message", "Hello Lambda!");
-//        System.out.println(new App().handleRequest(data));
-//    }
-//
-//    public String handleRequest(Map<String, String> data) {
-////        JSONObject jsonObject = new JSONObject();
-////        if (!data.containsKey("message")) {
-////            jsonObject.append("error", "Field message can not be null or empty");
-////            return jsonObject.toString();
-////        }
-////        StringBuilder sb = new StringBuilder(data.get("message"));
-////        for (int i = 1; i < sb.toString().toCharArray().length; i++) {
-////            sb.deleteCharAt(i);
-////        }
-////        jsonObject.put("message", sb.toString());
-////        System.out.println(jsonObject);
-////        System.out.println("Hello from my lambda function!");
-////        return jsonObject.toString();
-//        return null;
-//    }
-
 }
